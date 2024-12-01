44ff28b61f7cdd1a6f51df42faf99537aebf79bf
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-12-02 00:18:46 +0530

    Configure store with articleApi

---
+++ frontend/ai-article-summarizer/src/helper/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { articleApi } from "./apiService";
+
+export const store = configureStore({
+  reducer: {
+    [articleApi.reducerPath]: articleApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(articleApi.middleware),
+});
